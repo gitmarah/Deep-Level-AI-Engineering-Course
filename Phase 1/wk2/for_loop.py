1296da6c19b57acad8090dc10cf2be4ce967ddea
gitmarah <thetelastudio@gmail.com>
2026-04-23 10:43:45 +0000

    Understand For-Loop

---
+++ Phase 1/wk2/for_loop.py
@@ -0,0 +1,7 @@
+hoods = [
+    "Yoni", "Yelisanda", "American Embassy", "Red Stone", "O-block",
+    "Metchem", "Battalion", "Compton", "Rawdon St.", "Brookfields"]
+
+print("\n==== HOODS ====")
+for hood in hoods:
+    print(hood)
